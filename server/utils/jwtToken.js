549a8e4f07ce331dd65a420fe9cbceed1f66a63b
nachiketsoni <nxchikxt@gmail.com>
2025-11-26 03:17:44 +0530

    fix: remove secure and httpOnly options from cookie settings in jwtToken.js

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -8,9 +8,7 @@ export const sendToken=(user,statusCode,res,req)=>{
     const options={
         expires:new Date(
             Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000
-        ),
-        secure:process.env.NODE_ENV==="production"?true:false,
-        httpOnly:process.env.NODE_ENV==="production"?false:true,
+        )
     };
     res.status(statusCode).cookie("token",token,options).json({
         success:true,
