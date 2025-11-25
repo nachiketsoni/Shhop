3df218661cca831ab2631466e0d4cbfcef32fa30
nachiketsoni <nxchikxt@gmail.com>
2025-11-26 03:14:01 +0530

    fix: update main entry point and start script in package.json; modify cookie options in jwtToken.js for production environment

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -9,7 +9,8 @@ export const sendToken=(user,statusCode,res,req)=>{
         expires:new Date(
             Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000
         ),
-        httpOnly:true,
+        secure:process.env.NODE_ENV==="production"?true:false,
+        httpOnly:process.env.NODE_ENV==="production"?false:true,
     };
     res.status(statusCode).cookie("token",token,options).json({
         success:true,
